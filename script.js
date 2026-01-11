6d258084e7b11da660880ee4e577c86ae904bd2a
Aji permana sidik <ajipermanasidik37@gmail.com>
2026-01-11 17:24:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// ===== AOS Init =====
-AOS.init({
-  duration: 800,
-  once: true
-});
-
-// ===== Dark / Light Mode =====
 const toggleBtn = document.getElementById("themeToggle");
 const body = document.body;
 
-// load saved theme
+// load theme
 if(localStorage.getItem("theme") === "light"){
   body.classList.add("light");
   toggleBtn.textContent = "☀️";
@@ -24,10 +17,4 @@ toggleBtn.addEventListener("click", () => {
     toggleBtn.textContent = "🌙";
     localStorage.setItem("theme","dark");
   }
-});
-
-// ===== Form Demo =====
-document.querySelector("form").addEventListener("submit", e => {
-  e.preventDefault();
-  alert("Pesan terkirim! 🚀");
 });
